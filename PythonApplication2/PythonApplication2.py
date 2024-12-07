2d55a78e7430ec93d044579a23f7b8f002d90785
Acsard <temraire117@gmail.com>
2024-12-08 02:24:56 +0900

    get correlation coefficient.

--- PythonApplication2/PythonApplication2.py
+++ PythonApplication2/PythonApplication2.py
@@ -20,25 +20,67 @@ temperature_df = pd.read_csv('./dataFile2.csv', header = 6, encoding = result['e
 # change date to datetime.
 treatment_df['요양개시일'] = pd.to_datetime(treatment_df['요양개시일'])
 
+
 temperature_df['날짜'] = pd.to_datetime(temperature_df['날짜'])
 
-# add daily temperature range.
+
+
+# add a new column to calculate the difference in average temperature and daily temperature change.
+temperature_df['전일평균기온'] =  temperature_df['평균기온(℃)'].shift(1)
+
+temperature_df['평균기온차'] = (temperature_df['평균기온(℃)'] - temperature_df['전일평균기온']).abs()
+
 temperature_df['일교차'] = temperature_df['최고기온(℃)'] - temperature_df['최저기온(℃)']
 
 # Put the data together on a weekly basis.
-treatment_df['주간별진료'] = treatment_df['요양개시일'].dt.to_period('W')
+treatment_df['요양개시일'] = treatment_df['요양개시일'].dt.to_period('W')
 
-temperature_df['주간별기온'] = temperature_df['날짜'].dt.to_period('W')
+treatment_df['주간별진료'] = treatment_df.groupby('요양개시일')['진료에피소드건수'].transform('sum')
 
-# get add of all tretment count for weekly.
-weekly_Treatment_Data = treatment_df.groupby('주간별진료')['진료에피소드건수'].sum()
+treatment_df = treatment_df.drop(columns=['진료에피소드건수'])
 
-# get mean of average temperature and temperaturen range. 
-weekly_Temperature_Data = temperature_df.groupby('주간별기온').agg({
-    '평균기온(℃)': 'mean',
-    '일교차': 'mean'
-}).reset_index()
+treatment_df = treatment_df.drop_duplicates()
 
-print(weekly_Treatment_Data)
 
-print(weekly_Temperature_Data)
+temperature_df['날짜'] = temperature_df['날짜'].dt.to_period('W')
+
+temperature_df['주간평균기온차'] = temperature_df.groupby('날짜')['평균기온차'].transform('mean')
+
+temperature_df['주간일교차평균'] = temperature_df.groupby('날짜')['일교차'].transform('mean')
+
+# New dataframe for calculated.
+temperature_range_df = pd.DataFrame(columns = ['주간별', '주간일교차평균', '전주비교온도차'])
+
+
+# fill columns
+
+temperature_range_df['주간별'] = temperature_df['날짜']
+
+temperature_range_df['주간일교차평균'] = temperature_df['주간일교차평균']
+
+temperature_range_df['전주비교온도차'] = temperature_df['주간평균기온차'] 
+
+temperature_range_df = temperature_range_df.drop_duplicates()
+
+
+
+
+
+
+# Change column name.
+treatment_df = treatment_df.rename(columns={'요양개시일': '주간별'})
+
+
+# Concatenate two dataframes.
+combined_df = pd.concat([treatment_df, temperature_range_df], axis = 1)
+
+combined_df = combined_df.loc[:, ~combined_df.columns.duplicated()]
+
+
+corr1 = combined_df['주간별진료'].corr(combined_df['주간일교차평균'])
+corr2 = combined_df['주간별진료'].corr(combined_df['전주비교온도차'])
+
+print(combined_df)
+
+print(corr1)
+print(corr2)
